bf473b800064ae746a7f8a5b8e0e910edbca6c37
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-03-23 17:10:20 -0700

    Update RutasProtegidas.tsx

--- src/rutas/RutasProtegidas.tsx
+++ src/rutas/RutasProtegidas.tsx
@@ -2,12 +2,24 @@ import { Navigate, Outlet } from "react-router";
 import { usarSesion } from "@/global/usarSesion";
 
 export function RutaCliente() {
+  const modoDesarrollo = import.meta.env.DEV;
+
+  if (modoDesarrollo) {
+    return <Outlet />;
+  }
+
   const { id } = usarSesion();
   if (!id) return <Navigate to="/iniciar-sesion" replace />;
   return <Outlet />;
 }
 
 export function RutaAdmin() {
+  const modoDesarrollo = import.meta.env.DEV;
+
+  if (modoDesarrollo) {
+    return <Outlet />;
+  }
+
   const { id, esAdmin, modoAdmin } = usarSesion();
   if (!id) return <Navigate to="/iniciar-sesion" replace />;
   if (!esAdmin || !modoAdmin) return <Navigate to="/" replace />;
