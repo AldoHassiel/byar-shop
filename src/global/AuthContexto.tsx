e4e6ca1fca3c25615b88bb697ddb1f8a38417e27
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-03-26 02:15:20 -0700

    Login

---
+++ src/global/AuthContexto.tsx
@@ -0,0 +1,72 @@
+import { apiAuth } from "@/api/auth.api";
+import type { InicioSesion } from "@/types/auth";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from "react";
+
+interface ContextoAutenticacionTipo {
+  usuario: InicioSesion | null;
+  cargando: boolean;
+  iniciarSesion: (correo: string, pwd: string) => Promise<boolean>;
+  cerrarSesion: () => void;
+}
+
+export const ContextoAutenticacion =
+  createContext<ContextoAutenticacionTipo | null>(null);
+
+interface Props {
+  children: ReactNode;
+}
+
+export function ProveedorAutenticacion({ children }: Props) {
+  const [usuario, setUsuario] = useState<InicioSesion>(null);
+  const [cargando, setCargando] = useState(true);
+
+  useEffect(() => {
+    const usuarioGuardado = localStorage.getItem("usuario");
+
+    if (usuarioGuardado) {
+      setUsuario(JSON.parse(usuarioGuardado));
+    }
+    setCargando(false);
+  }, []);
+
+  const iniciarSesion = async (correo: string, pwd: string) => {
+    setCargando(true);
+    const respuesta = await apiAuth.inciarSesion(correo, pwd, true);
+    if (respuesta.estado) {
+      setUsuario(respuesta.datos[0]);
+      localStorage.setItem("usuario", JSON.stringify(respuesta.datos[0]));
+    }
+    setCargando(false);
+    return respuesta.estado;
+  };
+
+  const cerrarSesion = () => {
+    setCargando(true);
+    setUsuario(null);
+    localStorage.removeItem("usuario");
+    setCargando(false);
+  };
+
+  return (
+    <ContextoAutenticacion.Provider
+      value={{ usuario, cargando, iniciarSesion, cerrarSesion }}
+    >
+      {children}
+    </ContextoAutenticacion.Provider>
+  );
+}
+
+export function useAutenticacion() {
+  const contexto = useContext(ContextoAutenticacion);
+  if (!contexto)
+    throw new Error(
+      "useAutenticacion debe usarse dentro de <ProveedorAutenticacion>",
+    );
+  return contexto;
+}
--- src/rutas/RutasProtegidas.tsx
+++ src/rutas/RutasProtegidas.tsx
@@ -1,21 +1,26 @@
-import { Outlet } from "react-router";
+import { useAutenticacion } from "@/global/AuthContexto";
+import { Navigate, Outlet } from "react-router";
 
 export function RutaCliente() {
+  const { usuario } = useAutenticacion();
   const modoDesarrollo = import.meta.env.DEV;
 
   if (modoDesarrollo) {
     return <Outlet />;
   }
 
+  if (!usuario?.id) return <Navigate to="/iniciar-sesion" replace />;
   return <Outlet />;
 }
 
 export function RutaAdmin() {
+  const { usuario } = useAutenticacion();
   const modoDesarrollo = import.meta.env.DEV;
 
   if (modoDesarrollo) {
     return <Outlet />;
   }
 
+  if (!usuario?.es_admin) return <Navigate to="/iniciar-sesion" replace />;
   return <Outlet />;
 }
