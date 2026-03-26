710087adfd2b6c7ccf769c826aa3c1278b019443
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-03-25 18:29:39 -0700

    Elimina usarSesion

--- src/rutas/RutasProtegidas.tsx
+++ src/rutas/RutasProtegidas.tsx
@@ -1,27 +1,21 @@
-import { Navigate, Outlet } from "react-router";
-import { usarSesion } from "@/global/usarSesion";
+import { Outlet } from "react-router";
 
 export function RutaCliente() {
-  const { id } = usarSesion();
   const modoDesarrollo = import.meta.env.DEV;
 
   if (modoDesarrollo) {
     return <Outlet />;
   }
 
-  if (!id) return <Navigate to="/iniciar-sesion" replace />;
   return <Outlet />;
 }
 
 export function RutaAdmin() {
-  const { id, esAdmin, modoAdmin } = usarSesion();
   const modoDesarrollo = import.meta.env.DEV;
 
   if (modoDesarrollo) {
     return <Outlet />;
   }
 
-  if (!id) return <Navigate to="/iniciar-sesion" replace />;
-  if (!esAdmin || !modoAdmin) return <Navigate to="/" replace />;
   return <Outlet />;
 }
