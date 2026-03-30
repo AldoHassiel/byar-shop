d42c1f6800beecf92db1cf9976f273edd4fc4ba3
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-03-29 19:10:22 -0700

    Crea el hook useCarrito

---
+++ src/hooks/useCarrito.tsx
@@ -0,0 +1,93 @@
+import { apiCarrito } from "@/api/carrito";
+import { useAutenticacion } from "@/global/AuthContexto";
+import type { Carrito } from "@/types/carrito";
+import { useEffect, useRef, useState } from "react";
+
+export default function useCarrito(idDireccionInicial?: number) {
+  const [carrito, setCarrito] = useState<Carrito>();
+  const idDireccionRef = useRef<number | undefined>(idDireccionInicial);
+  const [cargando, setCargando] = useState(false);
+
+  const { usuario } = useAutenticacion();
+
+  const recargarCarrito = async (
+    idDireccion?: number,
+    mostrarNotificacion = true,
+  ) => {
+    const datos = await apiCarrito.obtenerCarrito(
+      usuario?.id,
+      idDireccion,
+      mostrarNotificacion,
+    );
+    if (datos) setCarrito(datos);
+  };
+
+  const obtenerCarrito = async (
+    idDireccion?: number,
+    mostrarNotificacion: boolean = true,
+  ) => {
+    idDireccionRef.current = idDireccion;
+    setCargando(true);
+    await recargarCarrito(idDireccion, mostrarNotificacion);
+    setCargando(false);
+  };
+
+  const agregarAlCarrito = async (
+    idProducto: number,
+    cantidad: number,
+    mostrarNotificacion: boolean = true,
+  ) => {
+    setCargando(true);
+    await apiCarrito.agregarAlCarrito(
+      usuario?.id,
+      idProducto,
+      cantidad,
+      mostrarNotificacion,
+    );
+    await recargarCarrito(idDireccionRef.current, false);
+    setCargando(false);
+  };
+
+  const actualizarCantidad = async (
+    idProducto: number,
+    delta: number,
+    mostrarNotificacion: boolean = true,
+  ) => {
+    setCargando(true);
+    await apiCarrito.actualizarCantidad(
+      usuario?.id,
+      idProducto,
+      delta,
+      mostrarNotificacion,
+    );
+    await recargarCarrito(idDireccionRef.current, false);
+    setCargando(false);
+  };
+
+  const eliminarProducto = async (
+    idProducto: number,
+    mostrarNotificacion: boolean = true,
+  ) => {
+    setCargando(true);
+    await apiCarrito.eliminarProducto(
+      usuario?.id,
+      idProducto,
+      mostrarNotificacion,
+    );
+    await recargarCarrito(idDireccionRef.current, false);
+    setCargando(false);
+  };
+
+  useEffect(() => {
+    obtenerCarrito(idDireccionRef.current);
+  }, []);
+
+  return {
+    carrito,
+    cargando,
+    obtenerCarrito,
+    agregarAlCarrito,
+    actualizarCantidad,
+    eliminarProducto,
+  };
+}
