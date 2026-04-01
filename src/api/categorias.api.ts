4e3c2613c85dc12202ef7eded66840d481c3ae93
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-04-01 15:30:11 -0700

    Crea la API para crear, editar y eliminar categorias

--- src/api/categorias.api.ts
+++ src/api/categorias.api.ts
@@ -1,37 +1,150 @@
 import type { ApiRespuesta } from "@/types/api";
 import api from "./api.config";
-import type {
-    Categorias,
-    //CategoriaDTO
-} from "@/types/categorias";
+import type { CategoriaDTO, Categorias } from "@/types/categorias";
 import { toast } from "sonner";
-
+import axios from "axios";
+import { mostrarErroresZod } from "@/lib/validaciones.zod";
 
 const obtenerTodas = async (mostrarNotificacion?: true) => {
-    try {
-        const respuestaHttp = await api.get<ApiRespuesta<Categorias>>("/categorias");
+  try {
+    const respuestaHttp =
+      await api.get<ApiRespuesta<Categorias>>("/categorias");
 
-        const respuestaAPI = respuestaHttp.data;
+    const respuestaAPI = respuestaHttp.data;
 
-        if (!respuestaAPI.estado) {
-            toast(respuestaAPI.mensaje, { position: "bottom-right" });
-            return null;
-        }
-
-        if (respuestaAPI.estado && mostrarNotificacion) {
-            toast(respuestaAPI.mensaje, { position: "bottom-right" });
-        }
-
-        return respuestaAPI.datos;
-    } catch (error) {
-        console.error(error);
-        toast("No se pudo hacer la petición", { position: "bottom-right" });
-
-        return null;
+    if (!respuestaAPI.estado) {
+      toast(respuestaAPI.mensaje, { position: "bottom-right" });
+      return null;
     }
-}
 
+    if (respuestaAPI.estado && mostrarNotificacion) {
+      toast(respuestaAPI.mensaje, { position: "bottom-right" });
+    }
+
+    return respuestaAPI.datos;
+  } catch (error) {
+    console.error(error);
+    toast("No se pudo hacer la petición", { position: "bottom-right" });
+
+    return null;
+  }
+};
+
+const crear = async (
+  datos: CategoriaDTO,
+  mostrarNotificacion: boolean = true,
+) => {
+  try {
+    const respuestaHttp = await api.post<ApiRespuesta<null>>("/categorias", {
+      nombre: datos.nombre,
+      descripcion: datos.descripcion,
+    });
+
+    const respuestaAPI = respuestaHttp.data;
+
+    if (respuestaAPI.estado && mostrarNotificacion) {
+      toast.success(respuestaAPI.mensaje, {
+        duration: 4000,
+      });
+    }
+
+    return respuestaAPI.estado;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const { mensaje, datos } = error.response.data;
+      if (mensaje === "Error de validaciones") {
+        mostrarErroresZod(datos);
+        return null;
+      }
+
+      toast.error(mensaje, {
+        duration: 4000,
+      });
+    }
+    return null;
+  }
+};
+
+const editar = async (
+  datos: CategoriaDTO,
+  mostrarNotificacion: boolean = true,
+) => {
+  if (!datos.id) {
+    toast.error("Falta el id de la categoría a editar", {
+      duration: 4000,
+    });
+
+    return null;
+  }
+
+  try {
+    const respuestaHttp = await api.put<ApiRespuesta<null>>(
+      `/categorias/${datos.id}`,
+      {
+        nombre: datos.nombre,
+        descripcion: datos.descripcion,
+      },
+    );
+
+    const respuestaAPI = respuestaHttp.data;
+
+    if (respuestaAPI.estado && mostrarNotificacion) {
+      toast.success(respuestaAPI.mensaje, {
+        duration: 4000,
+      });
+    }
+
+    return respuestaAPI.estado;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const { mensaje, datos } = error.response.data;
+      if (mensaje === "Error de validaciones") {
+        mostrarErroresZod(datos);
+        return null;
+      }
+
+      toast.error(mensaje, {
+        duration: 4000,
+      });
+    }
+    return null;
+  }
+};
+
+const eliminar = async (id: number, mostrarNotificacion: boolean = true) => {
+  try {
+    const respuestaHttp = await api.delete<ApiRespuesta<null>>(
+      `/categorias/${id}`,
+    );
+
+    const respuestaAPI = respuestaHttp.data;
+
+    if (respuestaAPI.estado && mostrarNotificacion) {
+      toast.success(respuestaAPI.mensaje, {
+        duration: 4000,
+      });
+    }
+
+    return respuestaAPI.estado;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const { mensaje, datos } = error.response.data;
+      if (mensaje === "Error de validaciones") {
+        mostrarErroresZod(datos);
+        return null;
+      }
+
+      toast.error(mensaje, {
+        duration: 4000,
+      });
+    }
+    return null;
+  }
+};
 
 export const apiCategorias = {
-    obtenerTodas,
+  obtenerTodas,
+  crear,
+  editar,
+  eliminar,
 };
