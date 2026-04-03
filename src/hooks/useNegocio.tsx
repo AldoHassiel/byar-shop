4b5e7ecc8b9d6f170ee57d5e32642a7d9af72fd7
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-04-02 22:52:45 -0700

    El componente footer y la pagina principal se traen los datos del negocio dinamicamente

--- src/hooks/useNegocio.tsx
+++ src/hooks/useNegocio.tsx
@@ -1,25 +1,45 @@
 import { apiNegocio } from "@/api/negocio.api";
-import type { Negocio } from "@/types/negocio";
+import type {
+  EditarDatosNegocio,
+  EditarImagenesNegocio,
+  Negocio,
+} from "@/types/negocio";
 import { useEffect, useState } from "react";
 
 export default function useNegocio() {
   const [negocio, setNegocio] = useState<Negocio>();
   const [cargando, setCargando] = useState(false);
 
+  const recargar = async () => {
+    const respuesta = await apiNegocio.obtenerDatos();
+    if (respuesta) setNegocio(respuesta);
+  };
+
+  const obtenerDatosNegocio = async () => {
+    setCargando(true);
+    const respuesta = await apiNegocio.obtenerDatos();
+    if (respuesta) setNegocio(respuesta);
+    setCargando(false);
+  };
+
+  const editarDatosNegocio = async (
+    datos: EditarDatosNegocio,
+    imagenes?: EditarImagenesNegocio,
+    mostrarNotificacion: boolean = true,
+  ) => {
+    setCargando(true);
+    const respuesta = await apiNegocio.editar(
+      datos,
+      imagenes,
+      mostrarNotificacion,
+    );
+    if (respuesta) recargar();
+    setCargando(false);
+  };
+
   useEffect(() => {
-    const cargar = async () => {
-      setCargando(true);
-
-      const datos = await apiNegocio.obtenerDatos();
-      if (datos) {
-        setNegocio(datos);
-      }
-
-      setCargando(false);
-    };
-
-    cargar();
+    obtenerDatosNegocio();
   }, []);
 
-  return { negocio, cargando };
+  return { negocio, cargando, obtenerDatosNegocio, editarDatosNegocio };
 }
--- src/layouts/LayoutPrincipal.tsx
+++ src/layouts/LayoutPrincipal.tsx
@@ -13,7 +13,7 @@ export default function LayoutPrincipal() {
       <main>
         <Outlet context={{ negocio, cargando }} />
       </main>
-      {negocio && <Footer negocio={negocio} />}
+      <Footer negocio={negocio} />
       <Toaster />
     </div>
   );
