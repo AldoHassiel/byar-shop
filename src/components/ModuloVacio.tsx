02af4f0ac03cff91d9abdf7fe3a7d6032295531a
yuriana15 <23020076@uadeo.mx>
2026-04-07 22:28:41 -0700

    Modulo vacio

--- src/components/ModuloVacio.tsx
+++ src/components/ModuloVacio.tsx
@@ -6,7 +6,7 @@ interface Props {
 export default function ModuloVacio({ modulo }: Props) {
     return (
         <>
-            <div className="flex flex-col justify-center items-center h-90 gap-2">
+            <div className="flex flex-col justify-center items-center h-full gap-2">
                 <Info size={48} className="text-byar" />
                 <p className="text-byar text-2xl">Aún no hay {modulo}</p>
             </div>
