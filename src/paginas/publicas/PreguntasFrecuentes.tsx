0bd1a1d38fdf727f725b66ed7b813372f6feb609
Aldo Hassiel <aldohassielacosta@gmail.com>
2026-04-17 10:42:42 -0700

    Update PreguntasFrecuentes.tsx

--- src/paginas/publicas/PreguntasFrecuentes.tsx
+++ src/paginas/publicas/PreguntasFrecuentes.tsx
@@ -2,7 +2,7 @@ import {
   Accordion,
   AccordionContent,
   AccordionItem,
-  AccordionTrigger
+  AccordionTrigger,
 } from "../../components/ui/accordion";
 
 const preguntas = [
@@ -14,7 +14,7 @@ const preguntas = [
   },
   {
     id: "item-2",
-    pregunta : "¿Cómo puedo cambiar mi correo o contraseña?",
+    pregunta: "¿Cómo puedo cambiar mi correo o contraseña?",
     respuesta:
       "Desde tu perfil entra a Mis datos y usa los botones Cambiar para actualizar correo o contraseña de forma segura.",
   },
@@ -53,22 +53,30 @@ const preguntas = [
     pregunta: "¿Cómo puedo eliminar mi cuenta?",
     respuesta:
       "Si deseas eliminar tu cuenta, por favor contacta con nuestro equipo de soporte a través del formulario de contacto en la sección Ayuda.",
-  }
+  },
 ];
 
 export default function PreguntasFrecuentes() {
   return (
     <div className="mt-26 items-center  ">
       <section className="max-w-4xl h-screen mx-auto px-4 mb-8">
-        <h1 className="text-4xl font-bold mb-4 text-center">Preguntas Frecuentes</h1>
-        <Accordion type="single" collapsible className="rounded-xl border px-4">
+        <h1 className="text-4xl font-bold mb-4 text-center">
+          Preguntas Frecuentes
+        </h1>
+        <Accordion
+          type="single"
+          collapsible
+          className="rounded-xl border px-4 "
+        >
           {preguntas.map((item) => (
             <AccordionItem key={item.id} value={item.id}>
-              <AccordionTrigger className="text-base font-semibold sm:text-lg">
+              <AccordionTrigger className="text-base font-semibold sm:text-lg cursor-pointer ">
                 {item.pregunta}
               </AccordionTrigger>
               <AccordionContent>
-                <p className="text-sm text-muted-foreground sm:text-base">{item.respuesta}</p>
+                <p className="text-sm text-muted-foreground sm:text-base">
+                  {item.respuesta}
+                </p>
               </AccordionContent>
             </AccordionItem>
           ))}
